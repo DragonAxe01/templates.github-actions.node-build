9dca8b312317daa44c1bb9b7795081d0323eaf46
aiisnotbad <olivier_ortiz@outlook.com>
2022-08-06 14:04:08 -0400

    pipeline secret test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,8 @@
 describe("Test suite 1", () => {
   test("Showing the injected secret from the pipeline", () => {
+    // prepare
+    const expected = "__this is secret1__";
+
     // run
     const secretValue: String | undefined = process.env.SECRET1;
 
@@ -7,7 +10,8 @@ describe("Test suite 1", () => {
     console.log("Secret injected:" + secretValue);
     expect(secretValue).toBeDefined();
     expect((secretValue as String).length > 0).toBeTruthy();
+    expect(secretValue as String).toEqual(expected);
   });
 });
 
-export {}
+export {};
